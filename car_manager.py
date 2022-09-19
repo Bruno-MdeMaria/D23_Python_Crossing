08c6025970aca2197432213f6107f31a5f7d33ba
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-19 00:39:16 -0300

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle, st
+import random
+
+CORES = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        todos_cars = []
+
+
+    def criar_carros(self):
+       novo_car = Turtle("square")
+       novo_car.penup()
+       novo_car.shapesize(stretch_wid=2, stretch_len=1)
+       novo_car.color(random.choice(CORES))
+    
