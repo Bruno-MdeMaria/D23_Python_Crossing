b42b95fb49ec81f53b611636b77ca5f0a37c09f8
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-09-20 23:37:00 -0300

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -9,7 +9,7 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        todos_cars = []
+        self.todos_cars = []
 
 
     def criar_carros(self):
@@ -17,4 +17,7 @@ class CarManager(Turtle):
        novo_car.penup()
        novo_car.shapesize(stretch_wid=2, stretch_len=1)
        novo_car.color(random.choice(CORES))
-    
+       randon_y  = random.randint(-250,250)   #posição no eixo x que o carro será criado
+       novo_car.goto(300, randon_y)
+       self.todos_cars.append(novo_car)
+
